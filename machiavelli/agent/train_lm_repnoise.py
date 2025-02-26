8f8368fd37f7b1bc82e8eb70dd75b33a6a75934e
Aniq Elahi <aniqelahi1@gmail.com>
2025-02-26 15:28:57 -0400

    Added Relative Path

--- machiavelli/agent/train_lm_repnoise.py
+++ machiavelli/agent/train_lm_repnoise.py
@@ -3,7 +3,9 @@ import torch
 import os
 import sys
 
-sys.path.append(r"/mnt/c/Users/aniqe/Desktop/Studies/BCS/NLP/Project/machiavelli")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "..", "..", ".."))
+sys.path.append(os.path.join(project_root, "machiavelli"))
 
 from transformers import AutoModelForTokenClassification, AutoModelForCausalLM
 
